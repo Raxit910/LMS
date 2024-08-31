8507758c1fe507781e306f5aa482170f1416b868
Raxit Sheladiya <raxitsheladiya@gmail.com>
2024-08-31 17:56:47 +0530

    first commit

---
+++ library.py
@@ -0,0 +1,16 @@
+class Book:
+    def __init__(self, isbn, title, author, year):
+        self.isbn = isbn
+        self.title = title
+        self.author = author
+        self.year = year
+        self.is_borrowed = False
+
+class Library:
+    def __init__(self):
+        self.books = []
+
+    def add_book(self, book):
+        if book in self.books:
+            raise ValueError("Book already exists in the library.")
+        self.books.append(book)
---
+++ test_library.py
@@ -0,0 +1,22 @@
+import unittest
+from library import Library, Book
+
+class TestLibrary(unittest.TestCase):
+
+    def setUp(self):
+        self.library = Library()
+
+    def test_add_book(self):
+        book = Book(isbn="1234567890", title="The Guide", author="RK Narayan", year=1958)
+        self.library.add_book(book)
+        self.assertIn(book, self.library.books)
+    
+    # Additional test cases for edge conditions
+    def test_add_duplicate_book(self):
+        book = Book(isbn="1234567890", title="The Guide", author="RK Narayan", year=1958)
+        self.library.add_book(book)
+        with self.assertRaises(ValueError):
+            self.library.add_book(book)  # Adding the same book again should raise an error
+
+if __name__ == "__main__":
+    unittest.main()
